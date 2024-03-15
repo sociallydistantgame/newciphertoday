2622b1a0858cb9a10b2878a71ab8f775e3cf591e
Ritchie Frodomar <alkalinethunder@gmail.com>
2024-03-14 22:08:05 -0400

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,53 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Socially Distant';
+export const SITE_LOGO = "/assets/logo-full.png";
+export const FOOTER_EXTRA = "All rights reserved. This project is under active development. Promotional content on this site is subject to change and may not reflect the final game.";
+export const SITE_DESCRIPTION = "Narrative-driven hacking game for PC set during a global pandemic";
+export const FOOTER_LOGO = "/assets/acidic-light-badge.png";
+export const FOOTER_LOGO_LINK = 'https://acidiclight.dev/';
+export const FOOTER_LOGO_ALT = "A game by acidic light";
+export const SITE_OWNER_URL = "https://acidiclight.dev/"
+export const COPYRIGHT_LEGAL_NAME = "Michael VanOverbeek";
+export const A11Y_ISSUE_LINK = "https://github.com/sociallydistantgame/sociallydistantgame.github.io/issues";
+export const HEADER_CTA_LINK = "/playtest"
+export const HEADER_CTA_TEXT = "Apply to playtest";
+
+export const SOCIAL_LINKS = [
+    {
+        name: 'GitHub',
+        link: 'https://github.com/sociallydistantgame',
+        icon: 'mdi:github'
+    },
+    {
+        name: 'Discord',
+        link: 'https://discord.gg/5qZUeeVvnT',
+        icon: 'mdi:discord'
+    },
+    {
+        name: 'Patreon',
+        link: 'https://patreon.com/acidiclight',
+        icon: 'mdi:patreon'
+    }
+]
+
+// TODO: Add support for child links
+export const MENU_LINKS = [
+    {
+        name: "Home",
+        href: "/"
+    },
+    {
+        name: "Devlogs",
+        href: "/devlogs"
+    },
+    {
+        name: "Forum",
+        href: "https://forum.sociallydistantgame.com/"
+    },
+    {
+        name: "Online Manual",
+        href: "https://man.sociallydistantgame.com/"
+    }
+];
---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"./node_modules/flowbite/**/*.js",
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
+	],
+	darkMode: 'class',
+	theme: {
+		fontFamily: {
+			'sans': ['Rajdhani', 'Rajdhani Bold', 'Rajdhani Light', 'Rajdhani SemiBold', 'Rajdhani Medium']
+		},
+		extend: {},
+	},
+	plugins: [
+		require('flowbite/plugin')
+	],
+}
